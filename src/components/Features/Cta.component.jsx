3330ac50c0f196eaaa10f3bacb9a5236383bc7a3
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-28 02:28:34 -0500

    bgImg for cta section

--- src/components/Features/Cta.component.jsx
+++ src/components/Features/Cta.component.jsx
@@ -8,15 +8,18 @@ const Cta = () => {
 
   return (
     <>
-      <div className={`${styles.cta} z-10 pt-6 flex flex-col items-center`}>
-        <h1 className="text-center text-4xl font-black">
+      <div
+        className={`${styles.cta} relative z-10 pt-6 flex flex-col items-center`}
+      >
+        <div className={`${styles.bgImg} z-0 absolute`}></div>
+        <h1 className="z-10 text-center text-4xl font-black">
           Bring everyone together to build better products.
         </h1>
-        <p className="pt-2.5 mb-8 text-center text-lg font-light text-gray-600">
+        <p className="z-10 pt-2.5 mb-8 text-center text-lg font-light text-gray-600">
           Manage makes it simple for software teams to plan day-to-day tasks
           while keeping the larger team goals in view.
         </p>
-        <Btn textContent="Get Started" />
+        <Btn className="z-10" textContent="Get Started" />
       </div>
     </>
   )
--- src/components/General/Btn.component.jsx
+++ src/components/General/Btn.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import styles from './styles/Btn.module.css'
 
-const Btn = ({ textContent, sm }) => {
+const Btn = ({ className, textContent, sm }) => {
   return (
     <button
-      className={` font-bold py-3 px-4 rounded-3xl duration-300 ${
+      className={`${className} font-bold py-3 px-4 rounded-3xl duration-300 ${
         sm ? styles.btnSm : styles.btn
       } bg-red-600 text-white hover:bg-red-300 hover:shadow-xl border-none`}
     >
